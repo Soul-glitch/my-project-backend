bdddc9efbd8817d0591357ac391958672fc11a14
NaCo2 <294566572@qq.com>
2023-12-05 11:01:33 +0800

    完善注册功能

--- src/main/java/com/example/entity/vo/request/EmailRegisterVO.java
+++ src/main/java/com/example/entity/vo/request/EmailRegisterVO.java
@@ -1,6 +1,7 @@
 package com.example.entity.vo.request;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
@@ -8,6 +9,7 @@ import org.hibernate.validator.constraints.Length;
 @Data
 public class EmailRegisterVO {
     @Email
+            @Length(min = 4)
     String email;
     @Length(max = 6,min = 6)
     String code;
